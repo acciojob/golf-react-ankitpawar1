20d7030285d62b526e3c49d9d33fc230e2b9f8dd
ankitpawar1 <ankitpawar192837@gmail.com>
2024-09-26 04:45:33 -0700

    fst

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,59 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false,
+      posi: 0,
+      ballPosition: { left: "0px" },
     };
+    this.renderChoice = this.renderBallOrButton.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
 
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+  }
+  handleKeyDown(event) {
+    if (event.key === "ArrowRight" || event.keyCode === 39) {
+      // Move ball 5px to the right when the right arrow is pressed
+      this.setState((prevState) => {
+        const newPos = prevState.posi + 5;
+        return {
+          posi: newPos,
+          ballPosition: { left: `${newPos}px` },
+        };
+      });
     }
+  }
+  //  1
+  renderBallOrButton() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else {
+      return (
+        <button onClick={this.buttonClickHandler} className="start">
+          Start
+        </button>
+      );
+    }
+  }
 
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
+  // bind ArrowRight keydown event
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    // Clean up event listener when component unmounts
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+  render() {
+    return <div className="playground">{this.renderBallOrButton()}</div>;
+  }
 }
-
 
 export default App;
